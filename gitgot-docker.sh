24e755869dadcf57e441810c83939ef6752b3bfd
Jake Miller <jmiller@bishopfox.com>
2019-07-23 09:35:29 -0400

    Added Docker Support

---
+++ gitgot-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -f "Dockerfile" && -f "gitgot.py" ]]; then
+    if [ -z $(docker images -q gitgot) ]; then
+        # Display output on fresh container build
+        docker build -t gitgot .
+    else
+        # Silent rebuild if in project directory
+        docker build -t gitgot . 2>&1 > /dev/null
+    fi
+else
+    echo "Not in project directory. Skipping container update/rebuild..."
+fi
+
+
+docker run --rm -it \
+    -e GITHUB_ACCESS_TOKEN=$GITHUB_ACCESS_TOKEN \
+    -v $PWD/logs:/gitgot/logs \
+    -v $PWD/states:/gitgot/states \
+    gitgot $@
